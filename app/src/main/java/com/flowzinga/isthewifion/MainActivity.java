dfea3a786b3ac8455a782ffcc63089f52f0508ef
Julien <jianhui@gmail.com>
2014-11-29 16:51:37 +0800

    a

--- app/src/main/java/com/flowzinga/isthewifion/MainActivity.java
+++ app/src/main/java/com/flowzinga/isthewifion/MainActivity.java
@@ -1,17 +1,76 @@
 package com.flowzinga.isthewifion;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    Button buttonCheck;
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCheck = (Button) findViewById(R.id.button1);
+        textView1 = (TextView) findViewById(R.id.textView1);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+
+        View.OnClickListener onClickListener1 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkWifi();
+            }
+        };
+
+        buttonCheck.setOnClickListener(onClickListener1);
+    }
+
+
+    public void checkWifi(){
+
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        if (!isConnected) {
+            textView1.setText("You are not connected to the internet.");
+            return;
+        }
+        else {
+            textView1.setText("You are connected to the internet.");
+        }
+
+
+        String a = activeNetwork.getTypeName();
+        textView2.setText("You are currently using " + a +  "data.");
+
+
+        if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
+            textView3.setText("Wifi connected.");
+        }
+        else
+            textView3.setText("Wifi not connected");
+
+
+            NetworkInfo netInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+            return ((netInfo != null) && netInfo.isConnected());
+        }
+
     }
 
 
@@ -30,9 +89,9 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
